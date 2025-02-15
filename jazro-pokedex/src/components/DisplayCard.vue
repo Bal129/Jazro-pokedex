be0e69673737f4a15c0bf193ea1625448a1bf2a9
Iqbal <a.iqbalcs129@gmail.com>
2025-02-15 20:01:19 +0800

    Update
    1. Created search functionality.
    2. Moved all fetch lines to parent - MainView.vue.
    3. Setup basic styling for MainView.vue.
    
    To do next:
    1. Find a way to properly fetch data in order.
    2. Vue Router - open a new page when click on a card.

--- jazro-pokedex/src/components/DisplayCard.vue
+++ jazro-pokedex/src/components/DisplayCard.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="card m-2">
         <h3 class="cus-index m-2">
-            #{{ cardData.id }}
+            #{{ props.pokemon.id }}
         </h3>
         <img 
             class="card-img-top" 
-            v-bind:src="cardData.sprite"
+            v-bind:src="props.pokemon.sprite"
             alt="Pokemon sprite"
         />
         <div class="card-body">
             <h3 class="card-title text-center py-1">
-                {{cardData.name}}
+                {{props.pokemon.name}}
             </h3>
             <div>
                 <ul 
@@ -18,7 +18,7 @@
                 >
                     <li 
                         class="list-group-item flex-fill text-center"
-                        v-for="row in cardData.types"
+                        v-for="row in props.pokemon.types"
                     >{{ row.type.name }}</li>
                 </ul>
             </div>
@@ -27,36 +27,37 @@
 </template>
 
 <script setup>
-import { ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 // import { useCounterStore } from '@/stores/counter';
-const props = defineProps(["id"]);
+const props = defineProps(["pokemon"]);
+
 // const pokemonId = 1;
 // const storeCounter = useCounterStore();
-const cardData = ref({
-    id:0,
-    name:"pokemon",
-    sprite: "n/a",
-    types: {},
-});
+// const cardData = ref({
+//     id:0,
+//     name:"pokemon",
+//     sprite: "n/a",
+//     types: {},
+// });
 
-onMounted(() => {
-    fetchData(props.id);
-});
+// onMounted(() => {
+//     fetchData(props.id);
+// });
 
-function fetchData(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(response => response.json())
-    .then(data => {
-        cardData.value = {
-            id: data.id,
-            name: capitalize(data.name),
-            sprite: data.sprites.front_default,
-            types: data.types
-        }
-        console.log(cardData.value);
-    })
-    .catch(error => console.error(error));
-}
+// function fetchData(id) {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         cardData.value = {
+//             id: data.id,
+//             name: capitalize(data.name),
+//             sprite: data.sprites.front_default,
+//             types: data.types
+//         }
+//         // console.log(cardData.value);
+//     })
+//     .catch(error => console.error(error));
+// }
 
 // function storeData(data) {
 //     // console.log(data);
@@ -72,9 +73,9 @@ function fetchData(id) {
 //     storeCounter.locations = data.locations;
 // }
 
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.substring(1);
-}
+// function capitalize(string) {
+//     return string.charAt(0).toUpperCase() + string.substring(1);
+// }
 
 </script>
 
--- jazro-pokedex/src/views/MainView.vue
+++ jazro-pokedex/src/views/MainView.vue
@@ -1,16 +1,126 @@
 <template>
-    <h1>Creating the display card...</h1>
-    <ul>
-        <li v-for="row in rows" :key="row">
-            <DisplayCard :id="row.toString()"/>
-        </li>
-    </ul>
+    <header class="py-2 bg-dark" id="navbarHeader">
+		<div class="container d-flex justify-content-between">
+			<h2 class="text-light">Logo</h2>
+            <input type="text" placeholder="Search..." v-model="target"/>
+			
+			<!-- <div class="row">
+				<div class="col-4">
+					<h2 class="text-light">Logo</h2>
+				</div>
+				<div class="col-8">
+					<h2 class="text-light float-right">Search Bar</h2>
+				</div>
+			</div> -->
+		</div>
+	</header>
     
+    <section>
+        <div class="container bg-light">
+            <div class="row py-3">
+                <h1 class="text-center">Welcome</h1>
+                <h3 class="text-muted text-center">This is a list of Pokemons from index 1 to {{numOfPokemon}}</h3>
+            </div>
+            <div class="row py-3">
+                <div  
+                    class="col-12 col-md-6 col-lg-3"
+                    v-for="pokemon in searchPokemon" :key="pokemon.id"
+                >
+                    <DisplayCard 
+                        :pokemon="pokemon"
+                    />
+                </div>
+            </div>
+            <!-- <ul class="list-group">
+                <li  
+                    class="list-group"
+                    v-for="pokemon in searchPokemon" :key="pokemon.id"
+                >
+                    <DisplayCard 
+                        :pokemon="pokemon"
+                    />
+                </li>
+            </ul> -->
+        </div>
+    </section>
+
+    <footer>
+        <div class="bg-dark text-muted">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+                    <p class="text-light p-4">
+                        This is a small project created by 
+                        <i>Ahmad Iqbal Bin Che Shamshudin</i>
+                        for <i>Robot Resources Front-End Challenges</i>
+                        Visit my <a href="https://github.com/Bal129">Github</a> for more projects
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 </template>
 
 <script setup>
+import { ref, onMounted, computed } from "vue";
 import DisplayCard from "../components/DisplayCard.vue";
 
-const rows = 100;
+const numOfPokemon = 100;
+
+const allPokemonData = ref([]);
+const pokemonData = ref({
+    id: "0",
+    name: "Name",
+    sprite: "Sprite",
+    types: {}
+});
+
+onMounted(() => {
+    fetchAllData();
+});
+
+function fetchAllData() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${numOfPokemon}`)
+    .then(response => response.json())
+    .then(allData => {
+        const promises = allData.results.map(eachData => 
+            fetchEachData(eachData)
+        );
+        Promise.all(promises);
+
+        // allData.results.forEach(eachData => {
+        //     fetchEachData(eachData)
+        // })
+    })
+    .catch(error => console.error("Error during fetch all data: " + error));
+}
+
+function fetchEachData(eachData) {
+    let link = eachData.url;
+
+    return fetch(link)
+    .then(response => response.json())
+    .then(data => {
+        pokemonData.value = {
+            id: data.id,
+            name: capitalize(data.name),
+            sprite: data.sprites.front_default,
+            types: data.types
+        }
+        allPokemonData.value.push(pokemonData.value);
+    })
+    .catch(error => console.error("Error during fetch each data: " + error));
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1);
+}
+
+const target = ref("");
+const searchPokemon = computed(() => {
+    return allPokemonData.value.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(target.value.toLowerCase())
+    );
+});
 
 </script>
